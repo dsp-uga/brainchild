0722b215e2a5772a4428faa1ab322a8feb967b77
raj-sivakumar <raji.a.siva@gmail.com>
2018-04-27 21:52:39 -0400

    final models and paper

--- brainchild/__main__.py
+++ brainchild/__main__.py
@@ -1,4 +1,35 @@
+import brainchild.loader
+import brainchild.reg_net as brn
+import torch.nn as N
+import torch
+
 def main():
-    
+    data_dir="/home/orcsy/brainchild/data/Mindboggle"
+    m = brn.reg_net()
+
+    if torch.cuda.is_available():
+        net = N.DataParallel(m, device_ids=range(0,len(torch.cuda.device_count())))
+        loader = iter(brainchild.loader.load_dataset(data_dir, dataset='Mindboggle', goal='register', batch_size=torch.cuda.device_count()))
+    else:
+        net = m
+        loader = iter(brainchild.loader.load_dataset(data_dir, dataset='Mindboggle', goal='register'))
+
+    loss = N.KLDivLoss()
+    optim = torch.optim.Adam(net.parameters())
+    reg = brn.GenerateRegistration(net=net, loss=loss, optim=optim)
+
+
+    # print(f'Epoch: {i+1}')
+    x, y = next(loader)
+    reg.partial_fit(x, y)
+
+    torch.save(net, "./network1.pt")
+    torch.save(reg, "./model1.pt")
+
+    load_test = iter(brainchild.loader.load_dataset(data_dir, dataset='Mindboggle', goal='register'))
+    x_test, y_test = next(load_test)
+    h_test = reg.predict(x_test)
+    torch.save(net, "./network.pt")
+    torch.save(reg, "./model.pt")
 if __name__ == '__main__':
     main()
--- brainchild/loader.py
+++ brainchild/loader.py
@@ -31,13 +31,13 @@ class MindboggleData(Dataset):
         img = nibabel.load(meta['path'])
         x = img.get_fdata(caching='unchanged')
         x = zero_pad_array(x, 256, 256, 256)
-        x = np.reshape(x,(1,256,256,256))
+        # x = np.reshape(x,(1,256,256,256))
         x = torch.from_numpy(x)
-        
+
         trgt_img = nibabel.load(meta['label'])
         y = trgt_img.get_fdata(caching='unchanged')
         y = zero_pad_array(y, 256, 256, 256)
-        y = np.reshape(y, (1,256,256,256))
+        # y = np.reshape(y, (1,256,256,256))
         y = torch.from_numpy(y)
         return x, y
 def zero_pad_array(array, d, w, h):
@@ -65,24 +65,24 @@ def zero_pad_array(array, d, w, h):
     if d_x%2 ==0:
         pad_x1 = pad_x2 = d_x//2
     else:
-        pad_x1 = math.floor(d_x//2)
-        pad_x2 = math.ceil(d_x//2)
+        pad_x1 = math.floor(d_x/2)
+        pad_x2 = math.ceil(d_x/2)
 
     if d_y%2 ==0:
         pad_y1 = pad_y2 = d_y//2
     else:
-        pad_y1 = math.floor(d_y//2)
-        pad_y2 = math.ceil(d_y//2)
+        pad_y1 = math.floor(d_y/2)
+        pad_y2 = math.ceil(d_y/2)
 
     if d_z%2 ==0:
         pad_z1 = pad_z2 = d_z//2
     else:
-        pad_z1 = math.floor(d_z//2)
-        pad_z2 = math.ceil(d_z//2)
+        pad_z1 = math.floor(d_z/2)
+        pad_z2 = math.ceil(d_z/2)
 
     pad_width = ((pad_x1,pad_x2),(pad_y1,pad_y2),(pad_z1,pad_z2))
-    print(type(pad_width))
     ret = np.pad(array,pad_width, mode='constant', constant_values=0)
+    ret = np.array([list(ret)])
     return ret
 class PPMIData(Dataset):
     '''An implementation of the torch `Dataset` interface for PPMI dataset.
